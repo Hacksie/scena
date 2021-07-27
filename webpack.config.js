a471a0545b83fcd49ead2fa3cf13954f09cc8b36
Ben Wise <hackerxse@hotmail.com>
2021-07-27 16:53:16 +1000

    Pre firebase attempt

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
                 type: 'asset/resource',
 
             },
+            {
+                test: /\.txt/,
+                type: 'asset/source',
+            },
             // {
             //     test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
             //     loader: 'url-loader?limit=100000'
